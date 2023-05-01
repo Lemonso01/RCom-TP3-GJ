d1f3fd99245600698d3b43af117b7359f5e0aaef
Lemonso01 <127228168+Lemonso01@users.noreply.github.com>
2023-05-01 19:33:31 +0100

    Add files via upload

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -5,9 +5,12 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
-
-#define BAUDRATE B38400
-#define MODEMDEVICE "/dev/ttyS1"
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+//mudamos o baudrate para 9600
+#define BAUDRATE B9600
+#define MODEMDEVICE "/dev/ttyS11"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
@@ -16,15 +19,16 @@ volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
-    int fd,c, res;
+    int fd,c, res, res2;
     struct termios oldtio,newtio;
     char buf[255];
+    char buf2[255];
     int i, sum = 0, speed = 0;
-
+    // mudei para ttyS10 e ttyS11 para experimentar socat em casa
     if ( (argc < 2) ||
-         ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-          (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+         ((strcmp("/dev/ttyS10", argv[1])!=0) &&
+          (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
         exit(1);
     }
 
@@ -43,16 +47,17 @@ int main(int argc, char** argv)
         exit(-1);
     }
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+    bzero(&newtio, sizeof(newtio)); // coloca o tamanho do newtio 
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD; // flag de controlo
+    newtio.c_iflag = IGNPAR;  // flag de configuracao da recepcao 
+    newtio.c_oflag = 0; // flag de configuracao da transmissao 
 
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received, corresponde ao numero de caracteres
+                                de controlo mudado para ler de 1 a 1*/
 
 
 
@@ -62,9 +67,9 @@ int main(int argc, char** argv)
     */
 
 
-    tcflush(fd, TCIOFLUSH);
+    tcflush(fd, TCIOFLUSH); //flushes both input and output data
 
-    if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    if (tcsetattr(fd,TCSANOW,&newtio) == -1) { // 
         perror("tcsetattr");
         exit(-1);
     }
@@ -72,22 +77,36 @@ int main(int argc, char** argv)
     printf("New termios structure set\n");
 
 
-
+    //colocar 255 caracteres
     for (i = 0; i < 255; i++) {
-        buf[i] = 'a';
+        
+        buf[i] = getchar();
+        if(buf[i] == '!')
+            break;
     }
 
     /*testing*/
-    buf[25] = '\n';
+    //buf[10] = '!';
 
-    res = write(fd,buf,255);
+    res = write(fd,buf,i);
     printf("%d bytes written\n", res);
+
+    printf("Termios structure retrieved\n");
+
+    while (STOP==FALSE) {       /* loop for input */
+        res2 = read(fd,buf2,255);   /* returns after 1 char have been input */
+        buf2[res2]=0;               /* so we can printf... */
+        printf(":%s:%d\n", buf2, res2); 
+
+        if (buf[i] == '!') STOP=TRUE;
+    }
 
 
     /*
     O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar
     o indicado no guião
     */
+   // ./writenoncanonical "/dev/ttyS0"
 
 
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
